5bf12f6ade51ad3e04fe4e62a95b30a880e1f555
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2024-11-22 22:49:56 -0800

    make the app name match the roc file name

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const roc_src = b.option([]const u8, "app", "the roc application to build");
+    var roc_src = b.option([]const u8, "app", "the roc application to build");
 
     const build_roc = b.addExecutable(.{
         .name = "build_roc",
@@ -19,12 +19,11 @@ pub fn build(b: *std.Build) void {
     run_build_roc.stdio = .inherit;
     run_build_roc.has_side_effects = true;
 
-    if (roc_src) |val| {
-        run_build_roc.addFileArg(b.path(val));
-    } else {
-        const default_path = "examples/hello.roc";
-        run_build_roc.addFileArg(b.path(default_path));
+    const default_path = "examples/hello.roc";
+    if (roc_src == null) {
+        roc_src = default_path;
     }
+    run_build_roc.addFileArg(b.path(roc_src.?));
 
     switch (optimize) {
         .ReleaseFast, .ReleaseSafe => {
@@ -37,7 +36,8 @@ pub fn build(b: *std.Build) void {
     }
 
     const exe = b.addExecutable(.{
-        .name = "server",
+        // Make the executable name the roc app name minus `.roc`.
+        .name = std.fs.path.stem(roc_src.?),
         .root_source_file = b.path("platform/host/main.zig"),
         .target = target,
         .optimize = optimize,
